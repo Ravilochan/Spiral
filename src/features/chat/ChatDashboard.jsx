4b2e9935a3122b75cba9c7c82d7196aeef896609
AryanDlaw <aryan1999varma@gmail.com>
2021-06-10 18:44:17 +0530

    chat commit

--- src/features/chat/ChatDashboard.jsx
+++ src/features/chat/ChatDashboard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Grid, Segment, Input } from "semantic-ui-react";
 export default function ChatDashboard() {
-    return (
-        <div>
-            Hello Chats
-        </div>
-    )
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <h1>Chats</h1>
+        <Segment.Group raised>
+          <Segment>Top</Segment>
+          <Segment>Middle</Segment>
+          <Segment>Middle</Segment>
+          <Segment>Middle</Segment>
+          <Segment>Bottom</Segment>
+        </Segment.Group>
+      </Grid.Column>
+      <Grid.Column width={12}>
+        <h1>Chat</h1>
+        <Segment.Group>
+          <div>
+            <Segment color="red">Hi Ravi</Segment>
+            <Segment color="blue">Hey Aryan</Segment>
+            <Segment color="red">This Works</Segment>
+            <Segment color="blue">Yes it does</Segment>
+            <Segment>
+              <Input fluid icon="send" placeholder="Enter Text..." size="big" />
+            </Segment>
+          </div>
+        </Segment.Group>
+      </Grid.Column>
+    </Grid>
+  );
 }
